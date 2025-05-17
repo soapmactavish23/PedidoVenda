0030018e17577bee03ab6ea59b03e283bf3cf8ba
Henrick <ricknogueira1231@gmail.com>
2025-05-17 16:16:24 -0300

    3.3. Prototipando a página de login

--- src/main/java/com/hkprogrammer/pedidovenda/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.hkprogrammer.pedidovenda;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
