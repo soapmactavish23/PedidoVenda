ef20de12ede56ec8039b143e83b33b9445c523aa
Henrick <ricknogueira1231@gmail.com>
2025-05-20 21:04:01 -0300

    5.2. Prototipando a pesquisa de pedidos

---
+++ src/main/java/com/hkprogrammer/PesquisaPedidosBean.java
@@ -0,0 +1,24 @@
+package com.hkprogrammer;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaPedidosBean {
+
+    private List<Integer> pedidosFiltrados;
+
+    public PesquisaPedidosBean() {
+        pedidosFiltrados = new ArrayList<>();
+        for(int i = 0; i < 50; i++) {
+            pedidosFiltrados.add(i);
+        }
+    }
+
+    public List<Integer> getPedidosFiltrados() {
+        return pedidosFiltrados;
+    }
+}
