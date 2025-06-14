a3f65d6314138fb9eab01fff3a65bf010911c61a
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-06-14 15:38:29 -0300

    9.3. Logging de mensagens e erros com Log4J

--- src/main/java/com/hkprogrammer/util/jsf/JsfExceptionHandler.java
+++ src/main/java/com/hkprogrammer/util/jsf/JsfExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.hkprogrammer.util.jsf;
 
 import com.hkprogrammer.service.NegocioException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.faces.FacesException;
 import javax.faces.application.ViewExpiredException;
@@ -14,6 +16,8 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public class JsfExceptionHandler extends ExceptionHandlerWrapper {
+
+    private static Log log = LogFactory.getLog(JsfExceptionHandler.class);
 
     private ExceptionHandler wrapped;
 
@@ -48,6 +52,7 @@ public class JsfExceptionHandler extends ExceptionHandlerWrapper {
                     FacesUtil.addErrorMessage(negocioException.getMessage());
                 } else {
                     handled = true;
+                    log.error("Erro de sistema: " + exception.getMessage(), exception);
                     redirect("/Erro.xhtml");
                 }
             } finally {
