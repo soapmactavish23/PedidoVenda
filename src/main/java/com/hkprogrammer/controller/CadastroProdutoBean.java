f9ca0a3d9bdafa42e7287f03685b3a938ec63e50
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-07-31 15:10:15 -0300

    12.1. Criando um produtor CDI para injetar EntityManager

--- src/main/java/com/hkprogrammer/controller/CadastroProdutoBean.java
+++ src/main/java/com/hkprogrammer/controller/CadastroProdutoBean.java
@@ -1,11 +1,17 @@
 package com.hkprogrammer.controller;
 
+import com.hkprogrammer.model.Categoria;
 import com.hkprogrammer.model.Produto;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @ViewScoped
@@ -13,10 +19,21 @@ public class CadastroProdutoBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Inject
+    private EntityManager manager;
+
     private Produto produto;
+
+    private List<Categoria> categoriasRaizes;
 
     public CadastroProdutoBean() {
         produto = new Produto();
+    }
+
+    public void inicializar() {
+        System.out.println("Inicializando...");
+
+        categoriasRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
     }
 
     public void salvar() {
@@ -24,6 +41,10 @@ public class CadastroProdutoBean implements Serializable {
 
     public Produto getProduto() {
         return produto;
+    }
+
+    public List<Categoria> getCategoriasRaizes() {
+        return categoriasRaizes;
     }
 
 }
